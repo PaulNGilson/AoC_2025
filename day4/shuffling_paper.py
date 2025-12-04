a2d43ca7c8ee377c3fc7d880ffc6c54125063d9e
Paul Gilson <paul.gilson@makers.tech>
2025-12-04 17:27:00 +0000

    day4

--- day4/shuffling_paper.py
+++ day4/shuffling_paper.py
@@ -18,37 +18,38 @@ if len(sys.argv) > 1:
 
 data = open_input()
 
+# pad the grid with dots, just to make handling edges easier
 for i in range(0, len(data)):
     data[i] = "." + data[i] + "."
 data = ["."*len(data[0])] + data + ["."*len(data[0])]
 
 def accessible(x, y):
     neighbours = data[y-1][x-1:x+2] + data[y][x-1] + data[y][x+1] + data[y+1][x-1:x+2]
-    #print(x, y, neighbours, neighbours.count("@"), neighbours.count("@")<4)
     return neighbours.count("@") < 4
 
-accessible_count = 0
+accessible_count_part_1 = 0
 
 for y in range(1, len(data)-1):
     for x in range(1, len(data[0])-1):
         if data[y][x] == "@" and accessible(x, y):
-            accessible_count += 1
+            accessible_count_part_1 += 1
 
-assert accessible_count == 13 if TESTDATA else True
-print("part 1:", accessible_count)
+assert accessible_count_part_1 == 13 if TESTDATA else True
+print("part 1:", accessible_count_part_1)
 
 # part 2 begins
 
 accessible_count_part_2 = 0
-more_to_do = True
-while more_to_do:
-    more_to_do = False
+more_to_try = True
+
+while more_to_try:
+    more_to_try = False
     for y in range(1, len(data)-1):
         for x in range(1, len(data[0])-1):
             if data[y][x] == "@" and accessible(x, y):
                 accessible_count_part_2 += 1
                 data[y] = data[y][:x] + "." + data[y][x+1:]
-                more_to_do = True
+                more_to_try = True
 
 assert accessible_count_part_2 == 43 if TESTDATA else True
 print("part 2:", accessible_count_part_2)
